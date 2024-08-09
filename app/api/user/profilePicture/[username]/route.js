e568dfa2644acfdab3f401cea7b1a94967303538
Christopher Meyer <cameyer06@gmail.com>
2024-08-09 15:41:09 -0500

    some more work with aws done. began reworking it, still some left to do

--- app/api/user/profilePicture/[username]/route.js
+++ app/api/user/profilePicture/[username]/route.js
@@ -1,7 +1,6 @@
 import dbConnect from "../../../../../lib/db/mongodb.js";
 import VerifyUser from "../../../reusuableMethods/verifyUser.js";
 import JWTErrors from "../../../reusuableMethods/jwtErrors.js";
-import s3 from "../../../../../lib/db/awsConnect.js";
 
 export async function GET(req, context) {
     try {
@@ -13,32 +12,7 @@ export async function GET(req, context) {
             throw new Error();
         }
 
-        const { params } = context;
         
-        // check that it exists first
-        const searchParams = {
-            Bucket: "cmeyerbucket",
-            Key: params.username
-        };
-        let exists = null;
-        try {
-            exists = await s3.headObject(searchParams).promise();
-        } catch(error) {
-            if(error.code === "NotFound") {
-                exists = null;
-            } else throw new Error();
-        }
-        if(!exists) {
-            throw new Error("Profile picture not found.");
-        }
-
-        // now that we know it exists, we generate a presigned url that expires in 3 min and return it
-        const uploadParams = {
-            Bucket: "cmeyerbucket",
-            Key: params.username,
-            Expires: 60*3, // 3 minutes
-        };
-        const preSignedUrl = await s3.getSignedUrl("getObject", uploadParams);
         
         return new Response(JSON.stringify({ message: "Success.", url: preSignedUrl }), {
             headers: {
@@ -50,18 +24,6 @@ export async function GET(req, context) {
         console.log(error);
 
         JWTErrors(error);
-
-        if (error.message === "Profile picture not found.") {
-            return new Response(
-                JSON.stringify({ message: "Profile picture not found." }),
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    status: 404,
-                }
-            );
-        }
 
         return new Response(
             JSON.stringify({ message: "Internal server error." }),
--- app/api/user/profilePicture/route.js
+++ app/api/user/profilePicture/route.js
@@ -1,11 +1,16 @@
 import dbConnect from "../../../../lib/db/mongodb.js";
-import s3 from "../../../../lib/db/awsConnect.js";
 import VerifyUser from "../../reusuableMethods/verifyUser.js";
 import JWTErrors from "../../reusuableMethods/jwtErrors.js";
-const multer = require("multer");
-// save file in ram
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+// aws s3 stuff
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+    region: process.env.AWS_REGION,
+});
 
 export async function POST(req) {
     // upload new profile picture
@@ -16,33 +21,14 @@ export async function POST(req) {
         if (!user) {
             throw new Error();
         }
-        upload.single('image'); // tells multer we are expect one image file called image, it will throw an error if this is not the case
 
-        // first check if user already has a profile picture uploaded
-        const searchParams = {
-            Bucket: "cmeyerbucket",
-            Key: user.username
+        const params = {
+            Bucket: process.env.BUCKET_NAME,
+            Key: user._id.toString(),
+            Body: req.file.buffer,
+            ContentType: req.file.mimetype,
         }
-        let exists = null;
-        try {
-            exists = await s3.headObject(searchParams).promise();
-        } catch(error) {
-            if(error.code === "NotFound") {
-                exists = null;
-            } else throw new Error();
-        }
-        if(exists) {
-            // alter the already exists profile picture
-            console.log("exists is true");
-        } else {
-            // else upload new profile picture
-            const params = {
-                Bucket: "cmeyerbucket",
-                Key: user.username,
-                Body: req.file,
-            };
-            await s3.putObject(params).promise();
-        }
+        const command = new PutObjectCommand(params);
 
         return new Response(
             JSON.stringify({ message: "Successfully uploaded picture." }),
